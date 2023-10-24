4ff143ed41a68b650f36e0744b186209c82403ea
xDJICDx <j.van.der.heijden@sintlucasedu.nl>
2023-10-24 10:37:55 +0200

    TMP import

--- Assets/Scripts/SnailSpawner.cs
+++ Assets/Scripts/SnailSpawner.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SnailSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float spawnTimer1= 10;
+    public GameObject Snail1;
+    private bool TimerBool1 = false;
+    public int MaxEntityLimit = 20;
+    private int CurrentEntityCount = 0; 
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        
+        if (!TimerBool1 && CurrentEntityCount < MaxEntityLimit)
+        {
+            TimerBool1 = true;
+            Instantiate(Snail1);
+            CurrentEntityCount++;
+            StartCoroutine(DelaySnail1());
+        }
     }
+
+    public IEnumerator DelaySnail1()
+    {
+        yield return new WaitForSeconds(spawnTimer1);
+        TimerBool1 = false;
+    }
+
 }
