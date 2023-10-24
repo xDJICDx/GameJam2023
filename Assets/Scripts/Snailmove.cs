66b7877744ec03cdddc7c3bc3e1dcf1d22f80263
xDJICDx <j.van.der.heijden@sintlucasedu.nl>
2023-10-24 13:33:35 +0200

    changes snake movement

--- Assets/Scripts/Snailmove.cs
+++ Assets/Scripts/Snailmove.cs
@@ -8,8 +8,7 @@ using Random = UnityEngine.Random;
 
 public class Snailmove : MonoBehaviour
 {
-    public GameObject Snail;
-    private Vector2 XZPosition;
+    public GameObject Player;
     private float xPos;
     private float yPos;
     public Rigidbody2D rigidbody;
@@ -18,32 +17,61 @@ public class Snailmove : MonoBehaviour
     public float minY;
     public float maxY;
     public float Velocity = 1f;
+    public float range = 0.2f;
+    public bool snailGettingChased = false;
+    public float ChaseRanege = 1f;
+    public bool FirstLocation = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        minX = -4f;
-        maxX = 4f;
-        minY = -4f;
-        maxY = 4f;
-        newLocation();
+        Player = GameObject.FindWithTag("Player");
+        Velocity = 1f;
+        minX = -4.7f;
+        maxX = 4.8f;
+        minY = -3f;
+        maxY = 3f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x == xPos && transform.position.y == yPos)
+        if (!snailGettingChased)
         {
-            newLocation();
+            if(!FirstLocation || 
+                transform.position.x > xPos - range && transform.position.x < xPos + range &&
+                transform.position.y > yPos - range && transform.position.y < yPos + range) 
+            {
+                newLocation();
+                FirstLocation = true;
+            }
+            seeekLocation();
         }
-        XZPosition = new Vector2(transform.position.x, transform.position.y);
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(xPos,yPos), Velocity * Time.deltaTime);
 
+        if(Player.transform.position.x - range > transform.position.x && 
+           Player.transform.position.x + range < transform.position.x &&
+           Player.transform.position.y - range > transform.position.y &&
+           Player.transform.position.y + range < transform.position.y) 
+        {
+            Debug.Log("close to player");
+        }
+        else
+        {
+            Debug.lo
+        }
+
+    }
+    
+    void seeekLocation()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(xPos, yPos), Velocity * Time.deltaTime);
+        var direction = transform.position - new Vector3(xPos, yPos, 0);
+        transform.up = direction * -1;
     }
     
     void newLocation()
     {
-
         xPos = Random.Range(minX, maxX);
         yPos = Random.Range(minY, maxY);
     }
